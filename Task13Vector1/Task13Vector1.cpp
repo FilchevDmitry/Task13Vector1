e147d8889bb4003c9cc4363e6bdca920af3d03af
DmityFilchev <filchev_dmitry@mail.ru>
2022-04-11 12:45:43 +0300

    Добавьте файлы проекта.

---
+++ Task13Vector1/Task13Vector1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    vector<int> num;
+    int cou = 0;
+    cout << "Enter the length :";
+    cin >> cou;
+    for (int i = 0; i < cou; ++i) {
+        int temp = 0;
+        cout << "Input numbers " << i + 1 << "-";
+        cin >> temp;
+        num.push_back(temp);
+    }
+    for (int i = 0; i < num.size(); ++i) {
+        cout << num[i] << " ";
+    }
+    system("pause");
+    return 0;
+}
