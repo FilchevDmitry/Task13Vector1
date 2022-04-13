761b34eb636bdc65add84efd3d5e857eded828a7
DmityFilchev <filchev_dmitry@mail.ru>
2022-04-13 10:55:50 +0300

    '1'

--- Task13Vector1/Task13Vector1.cpp
+++ Task13Vector1/Task13Vector1.cpp
@@ -2,8 +2,7 @@
 #include <vector>
 using namespace std;
 int main() {
-    vector<int> num1;
-    vector<int>num2;
+    vector<int> num;
     int cou;
     int del = 0;
     cout << "Enter the length :";
@@ -12,18 +11,19 @@ int main() {
         int temp = 0;
         cout << "Input numbers " << i + 1 << "-";
         cin >> temp;
-        num1.push_back(temp);
+        num.push_back(temp);
     }
     cout << "Enter delete number =";
     cin >> del;
-    for (int i = 0; i < cou; i++)
+    for (int i = 0; i < num.size(); i++)
     {
-        if (num1.at(i) != del)
-            num2.push_back(num1.at(i));
+        if (num.at(i) == del) {
+            num.erase(num.begin() + i);
+            --i;
+        }
     }
-    for (int i = 0; i < num2.size(); ++i) {
-        cout << num2[i] << " ";
+    for (int i = 0; i < num.size(); ++i) {
+        cout << num[i] << " ";
     }
-
     return 0;
 }
